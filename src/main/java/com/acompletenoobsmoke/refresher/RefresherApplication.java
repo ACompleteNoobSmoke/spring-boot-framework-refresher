14dc8d5769bddd9645d815ced0fda3d7d550092b
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2025-10-02 09:50:24 -0500

    Worked on Object Mapper

--- src/main/java/com/acompletenoobsmoke/refresher/RefresherApplication.java
+++ src/main/java/com/acompletenoobsmoke/refresher/RefresherApplication.java
@@ -1,7 +1,11 @@
 package com.acompletenoobsmoke.refresher;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +15,17 @@ public class RefresherApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RefresherApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandlineRunner(ObjectMapper objectMapper) throws JsonProcessingException {
+        String personString = "{\"id\":1,\"name\":\"Rock Lee\",\"age\":18}";
+        Person person = objectMapper.readValue(personString, Person.class);
+        System.out.println(person);
+        System.out.println(objectMapper.writeValueAsString(person));
+        return args -> {
+
+        };
     }
 
 }
