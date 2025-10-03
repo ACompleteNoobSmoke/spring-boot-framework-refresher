2c38784e800a8e7423e5359f246898ce75674820
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2025-10-03 12:31:58 -0500

    Made fake repo

--- src/main/java/com/acompletenoobsmoke/refresher/book/BookFakeRepo.java
+++ src/main/java/com/acompletenoobsmoke/refresher/book/BookFakeRepo.java
@@ -14,60 +14,79 @@ public class BookFakeRepo implements BookDAO {
 
     @Override
     public Optional<Book> getBookByID(Integer bookID) {
-        return Optional.empty();
+        if (BookValidation.validateBookExistence(bookID, books))
+            return books.stream().filter(b -> b.getBookID().equals(bookID)).findAny();
+        throw new RuntimeException("Book With ID: " + bookID + " doesn't exist");
     }
 
     @Override
     public Optional<List<Book>> getAllBooks() {
-        return Optional.empty();
+        return Optional.of(books);
     }
 
     @Override
     public Optional<Book> getBookForUser(Integer userID, Integer bookID) {
-        return Optional.empty();
+        return books.stream()
+                .filter(book -> book.getPersonID().equals(userID) && book.getBookID().equals(bookID))
+                .findAny();
     }
 
     @Override
     public Optional<List<Book>> getAllBooksForUser(Integer userID) {
-        return Optional.empty();
+        return Optional.of(books.stream()
+                .filter(book -> book.getPersonID().equals(userID))
+                .findAny()
+                .stream()
+                .toList());
     }
 
     @Override
     public Book addBook(Book book) {
-        books.add(book);
-        return book;
+        Book newBook = new Book(bookID.getAndIncrement(), book.getTitle(), book.getAuthor());
+        books.add(newBook);
+        return newBook;
     }
 
     @Override
     public Book addBookForUser(Integer userID, Book book) {
-        books.add(book);
-        return null;
+        Book newBook = new Book(book.getBookID(), userID, book.getTitle(), book.getAuthor());
+        books.add(newBook);
+        return newBook;
     }
 
     @Override
     public Book updateBook(Integer bookID, Book book) {
-        return null;
+        if (BookValidation.validateBookExistence(bookID, books)) {
+            Book existingBook = getBookByID(bookID).get();
+            String author = (BookValidation.validateBookAuthor(bookID, book.getAuthor(), books)) ? existingBook.getAuthor() :  book.getAuthor();
+            String title = (BookValidation.validateBookTitle(bookID, book.getTitle(), books)) ? existingBook.getTitle() :  book.getTitle();
+            Book updatedBook = new Book(bookID, existingBook.getPersonID(), title, author);
+            books.remove(existingBook);
+            books.add(updatedBook);
+            return updatedBook;
+        }
+        throw new IllegalArgumentException("Update Incomplete");
     }
 
     @Override
     public Book updateBookForUser(Integer userID, Integer bookID, Book book) {
         if (BookValidation.validateUserID(userID, books)
                 && BookValidation.validateBookExistence(bookID, books)) {
-            Book exisingBook = books.stream().filter(book1 -> book1.getBookID().equals(bookID)
-                    && book1.getPersonID().equals(userID)).findFirst().get();
-            String author = (BookValidation.validateBookAuthor(bookID, book.getAuthor(), books)) ? exisingBook.getTitle() :  exisingBook.getAuthor();
+            Book exisingBook = getBookForUser(userID, bookID).get();
+            String author = (BookValidation.validateBookAuthor(bookID, book.getAuthor(), books)) ? exisingBook.getAuthor() :  book.getAuthor();
             String title = (BookValidation.validateBookTitle(bookID, book.getTitle(), books)) ? exisingBook.getTitle() :  book.getTitle();
             Book updatedBook = new Book(bookID, userID, title, author);
             books.remove(exisingBook);
             return updatedBook;
         }
-        return null;
+        throw new IllegalArgumentException("Update Incomplete");
     }
 
     @Override
     public void deleteBook(Integer bookID) {
         if(BookValidation.validateBookExistence(bookID, books))
             books.removeIf(book -> bookID.equals(book.getBookID()));
+        throw new RuntimeException("Delete incomplete -- Book With ID: " + bookID + " doesn't exist");
     }
 
     @Override
@@ -76,5 +95,6 @@ public class BookFakeRepo implements BookDAO {
                 && BookValidation.validateBookExistence(userID, books))
             books.removeIf(book -> bookID.equals(book.getBookID())
                  && userID.equals(book.getPersonID()));
+        throw new RuntimeException("Delete incomplete");
     }
 }
